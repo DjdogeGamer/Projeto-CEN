c9c1b33bbf662421742c9d16030670581b8736c6
Gustavo <ghco@cesar.school>
2023-04-25 19:57:51 +0100

    New aws credentials

--- src/testSQLITE.js
+++ src/testSQLITE.js
@@ -2,8 +2,8 @@ const SQLiteS3 = require('./SQLiteS3');
 
 const bucketName = 'sqlite-js-ispg-test';
 const fileName = 'db.sqlite';
-const accessKeyId = 'AKIA4ZDOYH5FWHZQYJHU';
-const secretAccessKey = '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';
+const accessKeyId = 'AKIA4ZDOYH5F6H3ASIGQ';
+const secretAccessKey = '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';
 
 const sql_query = 'SELECT * FROM tabela_teste';
 
