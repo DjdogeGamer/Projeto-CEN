7ba9c8a477bed3cf46a85fcb83e2c23a31e4cdff
Gustavo <ghco@cesar.school>
2023-05-01 21:26:24 +0100

    Game Scene Examples

--- src/index.js
+++ src/index.js
@@ -64,7 +64,7 @@ class ChooseGameScene extends Phaser.Scene {
         // Game 1
         this.add.rectangle(200, 350, 100, 50, 0xffffff)
             .setInteractive()
-            .on('pointerdown', function() {}, this)
+            .on('pointerdown', this.loadGame1, this)
             .setStrokeStyle(4, 0x000000);
 
         this.add.text(200, 350, 'Jogo 1', {fontFamily: 'Arial', fontSize: 24, color: '#000000'}).setOrigin(0.5);
@@ -72,7 +72,7 @@ class ChooseGameScene extends Phaser.Scene {
         // Game 3
         this.add.rectangle(600, 350, 100, 50, 0xffffff)
             .setInteractive()
-            .on('pointerdown', function() {}, this)
+            .on('pointerdown', this.loadGame3, this)
             .setStrokeStyle(4, 0x000000);
 
         this.add.text(600, 350, 'Jogo 3', {fontFamily: 'Arial', fontSize: 24, color: '#000000'}).setOrigin(0.5);
@@ -80,7 +80,7 @@ class ChooseGameScene extends Phaser.Scene {
         // Game 2
         this.add.rectangle(400, 350, 100, 50, 0xffffff)
             .setInteractive()
-            .on('pointerdown', function() {}, this)
+            .on('pointerdown', this.loadGame2, this)
             .setStrokeStyle(4, 0x000000);
 
         this.add.text(400, 350, 'Jogo 2', {fontFamily: 'Arial', fontSize: 24, color: '#000000'}).setOrigin(0.5);
@@ -97,6 +97,120 @@ class ChooseGameScene extends Phaser.Scene {
     loadMenuScene() {
         this.scene.start('Menu');
     }
+
+    loadGame1() {
+        this.scene.start('Game1');
+    }
+
+    loadGame2() {
+        this.scene.start('Game2');
+    }
+
+    loadGame3() {
+        this.scene.start('Game3');
+    }
+}
+
+class Game1 extends Phaser.Scene {
+    constructor() {
+        super({key: 'Game1'});
+    }
+
+    preload() {
+
+    }
+
+    create() {
+        // Menu background
+        this.add.image(400, 300, 'background');
+
+        // Menu Title
+        this.add.rectangle(400, 200, 550, 100, 0x000000)
+        this.add.text(400, 200, 'Jogo 1 Exemplo', {
+            fontFamily: 'Arial',
+            fontSize: 64,
+            color: '#ffffff'
+        }).setOrigin(0.5);
+
+        this.add.rectangle(400, 500, 200, 50, 0xffffff)
+            .setInteractive()
+            .on('pointerdown', this.loadChooseGameScene, this)
+            .setStrokeStyle(4, 0x000000);
+
+        this.add.text(400, 500, 'Voltar', {fontFamily: 'Arial', fontSize: 24, color: '#000000'}).setOrigin(0.5);
+    }
+
+    loadChooseGameScene() {
+        this.scene.start('Menu');
+    }
+}
+
+class Game2 extends Phaser.Scene {
+    constructor() {
+        super({key: 'Game2'});
+    }
+
+    preload() {
+
+    }
+
+    create() {
+        // Menu background
+        this.add.image(400, 300, 'background');
+
+        // Menu Title
+        this.add.rectangle(400, 200, 550, 100, 0x000000)
+        this.add.text(400, 200, 'Jogo 2 Exemplo', {
+            fontFamily: 'Arial',
+            fontSize: 64,
+            color: '#ffffff'
+        }).setOrigin(0.5);
+
+        this.add.rectangle(400, 500, 200, 50, 0xffffff)
+            .setInteractive()
+            .on('pointerdown', this.loadChooseGameScene, this)
+            .setStrokeStyle(4, 0x000000);
+
+        this.add.text(400, 500, 'Voltar', {fontFamily: 'Arial', fontSize: 24, color: '#000000'}).setOrigin(0.5);
+    }
+
+    loadChooseGameScene() {
+        this.scene.start('ChooseGameScene');
+    }
+}
+
+class Game3 extends Phaser.Scene {
+    constructor() {
+        super({key: 'Game3'});
+    }
+
+    preload() {
+
+    }
+
+    create() {
+        // Menu background
+        this.add.image(400, 300, 'background');
+
+        // Menu Title
+        this.add.rectangle(400, 200, 550, 100, 0x000000)
+        this.add.text(400, 200, 'Jogo 3 Exemplo', {
+            fontFamily: 'Arial',
+            fontSize: 64,
+            color: '#ffffff'
+        }).setOrigin(0.5);
+
+        this.add.rectangle(400, 500, 200, 50, 0xffffff)
+            .setInteractive()
+            .on('pointerdown', this.loadChooseGameScene, this)
+            .setStrokeStyle(4, 0x000000);
+
+        this.add.text(400, 500, 'Voltar', {fontFamily: 'Arial', fontSize: 24, color: '#000000'}).setOrigin(0.5);
+    }
+
+    loadChooseGameScene() {
+        this.scene.start('ChooseGameScene');
+    }
 }
 
 const config = {
@@ -104,7 +218,7 @@ const config = {
     parent: 'phaser-example',
     width: 800,
     height: 600,
-    scene: [Menu, ChooseGameScene]
+    scene: [Menu, ChooseGameScene, Game1, Game2, Game3]
 };
 
 const game = new Phaser.Game(config);
