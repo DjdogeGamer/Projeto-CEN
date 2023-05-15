a22ea6727d7c08eac6670d920534007ce60f9cfb
Daniel Reis <reis.maggot@gmail.com>
2023-05-15 18:55:34 +0100

    game 2 update

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import Phaser from 'phaser';
 import bgImg from './assets/menu_bg.jpg';
+import game2_1 from './assets/game2_1.png';
 
 class Menu extends Phaser.Scene {
     constructor() {
@@ -320,122 +321,118 @@ class Game2 extends Phaser.Scene {
     }
 
     create() {
-        // Background color
-        this.cameras.main.setBackgroundColor('#cccccc');
-
+        
+        // Nível 1 background
+        this.add.image(400, 300, 'game2_1');
+    
         // Email inbox title
         this.add.text(420, 50, 'Email inbox', {fontFamily: 'Arial', fontSize: 32, color: '#000000'}).setOrigin(0.5);
-
+    
         // Email list background
-        this.add.rectangle(400, 300, 600, 350, 0xffffff);
-
+        this.add.rectangle(400, 300, 450, 250, 0xffffff);
+    
         // Email list items
-        const email1 = this.add.text(120, 170, '1. You have won a prize!', {
-            fontFamily: 'Arial',
-            fontSize: 20,
-            color: '#000000'
-        });
-
+        const email1 = this.add.text(180, 220, '1. You have won a prize!', {fontFamily: 'Arial', fontSize: 20, color: '#000000'});
+    
         // Add button to email item
         const email1Button = this.add.graphics()
-            .fillStyle(0x00ff00)
-            .fillRoundedRect(575, 160, 60, 35, 15)
-            .setInteractive()
-            .on('pointerdown', function () {
-                const message = 'Congratulations! You have won a prize.';
-                this.add.text(400, 300, message, {fontFamily: 'Arial', fontSize: 24, color: '#000000'}).setOrigin(0.5);
-            }, this);
-        this.add.text(605, 177, 'Open', {fontFamily: 'Arial', fontSize: 16, color: '#ffffff'}).setOrigin(0.5);
-
-
-        const email2 = this.add.text(120, 210, '2. Urgent message from your bank', {
-            fontFamily: 'Arial',
-            fontSize: 20,
-            color: '#000000'
-        });
-
+      .fillStyle(0x00ff00)
+      .fillRoundedRect(555, 210, 60, 35, 15)
+      .setInteractive()
+      .on('pointerdown', function () {
+        const message = 'Congratulations! You have won a prize.';
+        this.dialogPlugin.prompt(message, ['OK']);
+      }, this);
+    
+    this.add.text(585, 227, 'Open', {fontFamily: 'Arial', fontSize: 16, color: '#ffffff'}).setOrigin(0.5);
+    
+    // Add the dialog plugin to the scene
+    this.dialogPlugin = this.plugins.get('rexDialog');
+    
+    
+        const email2 = this.add.text(180, 260, '2. Urgent message from your bank', {fontFamily: 'Arial', fontSize: 20, color: '#000000'});
+    
         // Add button to email item
-        const email2Button = this.add.rectangle(500, 210, 100, 40, 0x00ff00)
-            .setInteractive()
-            .on('pointerdown', function () {
-                const message = 'This is an urgent message from your bank.';
-                this.add.text(400, 300, message, {fontFamily: 'Arial', fontSize: 24, color: '#000000'}).setOrigin(0.5);
-            }, this);
-        this.add.text(500, 210, 'Click', {fontFamily: 'Arial', fontSize: 16, color: '#ffffff'}).setOrigin(0.5);
-
-        // Close button
-        this.add.rectangle(700, 50, 50, 50, 0xff0000).setInteractive().on('pointerdown', function () {
-            this.scene.start('MyGame');
+        const email2Button = this.add.graphics()
+        .fillStyle(0x00ff00)
+        .fillRoundedRect(555, 250, 60, 35, 15)
+        .setInteractive()
+        .on('pointerdown', function () {
+            const message = 'Your bank took your money, have a nice day!';
+            this.add.text(400, 300, message, {fontFamily: 'Arial', fontSize: 24, color: '#000000'}).setOrigin(0.5);
         }, this);
-        this.add.text(700, 50, 'X', {fontFamily: 'Arial', fontSize: 24, color: '#ffffff'}).setOrigin(0.5);
-
+        this.add.text(585, 267, 'Open', {fontFamily: 'Arial', fontSize: 16, color: '#ffffff'}).setOrigin(0.5);
+    
+    
+        const email3 = this.add.text(180, 300, '3. Important security update', {fontFamily: 'Arial', fontSize: 20, color: '#000000'});
+    
+        // Add button to email item
+        const email3Button = this.add.graphics()
+        .fillStyle(0x00ff00)
+        .fillRoundedRect(555, 290, 60, 35, 15)
+        .setInteractive()
+        .on('pointerdown', function () {
+            const message = 'Your bank took your money, have a nice day!';
+            this.add.text(400, 300, message, {fontFamily: 'Arial', fontSize: 24, color: '#000000'}).setOrigin(0.5);
+        }, this);
+        this.add.text(585, 306, 'Open', {fontFamily: 'Arial', fontSize: 16, color: '#ffffff'}).setOrigin(0.5);
+    
+    
+        const email4 = this.add.text(180, 340, '4. Your account has been compromised', {fontFamily: 'Arial', fontSize: 20, color: '#000000'});
+    
+        // Add button to email item
+        const email4Button = this.add.graphics()
+        .fillStyle(0x00ff00)
+        .fillRoundedRect(555, 330, 60, 35, 15)
+        .setInteractive()
+        .on('pointerdown', function () {
+            const message = 'Your bank took your money, have a nice day!';
+            this.add.text(400, 300, message, {fontFamily: 'Arial', fontSize: 24, color: '#000000'}).setOrigin(0.5);
+        }, this);
+        this.add.text(585, 346, 'Open', {fontFamily: 'Arial', fontSize: 16, color: '#ffffff'}).setOrigin(0.5);
+    
+    
+    
+        // BOTAO DE VOLTAR -> vai para choose game
         this.add.rectangle(400, 500, 200, 50, 0xffffff)
-            .setInteractive()
-            .on('pointerdown', this.loadChooseGameScene, this)
-            .setStrokeStyle(4, 0x000000);
-
+        .setInteractive()
+        .on('pointerdown', this.loadChooseGameScene, this)
+        .setStrokeStyle(4, 0x000000);
         this.add.text(400, 500, 'Voltar', {fontFamily: 'Arial', fontSize: 24, color: '#000000'}).setOrigin(0.5);
-
-        const email3 = this.add.text(120, 250, '3. Important security update', {
-            fontFamily: 'Arial',
-            fontSize: 20,
-            color: '#000000'
-        })
-            .setInteractive()
-            .on('pointerdown', function () {
-                // Code to handle clicking on email3
-            });
-        const email4 = this.add.text(120, 290, '4. Your account has been compromised', {
-            fontFamily: 'Arial',
-            fontSize: 20,
-            color: '#000000'
-        })
-            .setInteractive()
-            .on('pointerdown', function () {
-                // Code to handle clicking on email4
-            });
-
-        // Close button
+        // BOTAO DE X -> vai para choose game
         this.add.rectangle(700, 50, 50, 50, 0xff0000)
-            .setInteractive()
-            .on('pointerdown', function () {
-                this.scene.start('MyGame');
-            }, this);
+                .setInteractive()
+                .on('pointerdown', this.loadChooseGameScene, this)
+                .setStrokeStyle(4, 0x000000);
+            
         this.add.text(700, 50, 'X', {fontFamily: 'Arial', fontSize: 24, color: '#ffffff'}).setOrigin(0.5);
-
-        // Back button
-        this.add.rectangle(400, 500, 200, 50, 0xffffff)
-            .setInteractive()
-            .on('pointerdown', this.loadChooseGameScene, this)
-            .setStrokeStyle(4, 0x000000);
-        this.add.text(400, 500, 'Voltar', {fontFamily: 'Arial', fontSize: 24, color: '#000000'}).setOrigin(0.5);
-    }
-
-    loadChooseGameScene() {
-        this.scene.start('ChooseGameScene');
-    }
-
-    showPopup(title, message) {
-        // Create the popup background
-        const popupBackground = this.add.rectangle(400, 300, 500, 200, 0xffffff);
-        popupBackground.setStrokeStyle(4, 0x000000);
-
-        // Add the popup title and message
-        this.add.text(400, 250, title, {fontFamily: 'Arial', fontSize: 24, color: '#000000'}).setOrigin(0.5);
-        this.add.text(400, 300, message, {fontFamily: 'Arial', fontSize: 20, color: '#000000'}).setOrigin(0.5);
-
-        // Add the close button
-        const closeButton = this.add.rectangle(400, 350, 100, 50, 0xff0000)
-            .setInteractive()
-            .on('pointerdown', function () {
-                // Remove the popup from the scene
-                popupBackground.destroy();
-                popupTitle.destroy();
-                popupMessage.destroy();
-                closeButton.destroy();
-            }, this);
-        this.add.text(400, 350, 'Close', {fontFamily: 'Arial', fontSize: 20, color: '#ffffff'}).setOrigin(0.5);
-    }
+        }
+        
+        loadChooseGameScene() {
+            this.scene.start('ChooseGameScene');
+        }
+    
+        showPopup(title, message) {
+            // Create the popup background
+            const popupBackground = this.add.rectangle(400, 300, 500, 200, 0xffffff);
+            popupBackground.setStrokeStyle(4, 0x000000);
+        
+            // Add the popup title and message
+            this.add.text(400, 250, title, {fontFamily: 'Arial', fontSize: 24, color: '#000000'}).setOrigin(0.5);
+            this.add.text(400, 300, message, {fontFamily: 'Arial', fontSize: 20, color: '#000000'}).setOrigin(0.5);
+        
+            // Add the close button
+            const closeButton = this.add.rectangle(400, 350, 100, 50, 0xff0000)
+                .setInteractive()
+                .on('pointerdown', function () {
+                    // Remove the popup from the scene
+                    popupBackground.destroy();
+                    popupTitle.destroy();
+                    popupMessage.destroy();
+                    closeButton.destroy();
+                }, this);
+            this.add.text(400, 350, 'Close', {fontFamily: 'Arial', fontSize: 20, color: '#ffffff'}).setOrigin(0.5);
+        }
 }
 
 class Game3 extends Phaser.Scene {
